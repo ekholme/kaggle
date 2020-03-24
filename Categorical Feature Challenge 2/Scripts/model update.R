1f098245811eed035a53b1dfd10a06f045e2be27
ekholme <eric.ekholm@gmail.com>
2020-03-24 08:14:38 -0400

    glmnet model with ns time and interacts

--- Categorical Feature Challenge 2/Scripts/model update.R
+++ Categorical Feature Challenge 2/Scripts/model update.R
@@ -68,70 +68,12 @@ train <- train %>%
   mutate(ord_3 = map_dbl(ord_3, ~strtoi(charToRaw(.), 16L)),
          ord_4 = map_dbl(ord_4, ~strtoi(charToRaw(.), 16L)),
          ord_5 = map_dbl(ord_5, ~strtoi(charToRaw(.), 16L) %>%
-                           reduce(`+`))) %>%
-  mutate_at(vars(matches("ord|day|month")), as.numeric) %>%
-  mutate_at(vars(-matches("ord|day|month")), as.factor) %>%
+                                    reduce(`+`))) %>%
+  mutate_at(vars(matches("ord")), as.numeric) %>%
+  mutate_at(vars(-matches("ord")), as.factor) %>%
   mutate_if(is.factor, ~fct_explicit_na(., na_level = "Missing"))
 
-#setting our recipe
-preproc_recipe <- recipe(target ~ .,
-         data = train %>%
-                  select(-id)) %>%
-  step_meanimpute(matches("ord|day|month")) %>%
-  step_other(all_nominal(), threshold = .05) %>%
-  step_dummy(all_nominal(), -all_outcomes()) %>%
-  step_center(matches("ord|day|month")) %>%
-  step_scale(matches("ord|day|month")) %>%
-  step_ns(starts_with("day"), deg_free = 3) %>%
-  step_ns(starts_with("month"), deg_free = 3) %>%
-  step_interact(terms = ~matches("bin|ord|day|month"):matches("ord|bin|day|month"))
 
-
-
-#bootstrapping cv
-train_cv <- train %>%
-  vfold_cv(v = 5, strata = "target")
-
-
-glmnet_mod <- logistic_reg(
-  mode = "classification",
-  penalty = tune(),
-  mixture = tune()
-) %>%
-  set_engine("glmnet")
-
-#training the model
-glmnet_tuned_results <- tune_grid(
-  preproc_recipe,
-  model = glmnet_mod,
-  resamples = train_cv,
-  grid = 10,
-  metrics = metric_set(roc_auc)
-) 
-
-#extracting the best model parameters
-##can go back and rerun with 1se
-best_glmnet_params <- glmnet_tuned_results %>%
-  select_by_one_std_err(metric = "roc_auc", desc(penalty))
-
-#finalizing recipe
-final_rec <- finalize_recipe(preproc_recipe, best_glmnet_params)
-
-#getting final design matrix
-trn <- final_rec %>%
-  prep() %>%
-  juice()
-
-#finalizing the model & fitting
-glmnet_final_mod <- glmnet_mod %>%
-  finalize_model(parameters = best_glmnet_params) %>%
-  fit(target ~ ., data = trn)
-
-#cleaning up env a bit
-rm(train, train_cv)
-gc()
-
-#reading in and manipulating test data
 test <- vroom(here::here("Categorical Feature Challenge 2/Data/test.csv")) %>%
   mutate(ord_1 = str_replace_all(ord_1, c("Novice" = "0", "Contributor" = "1", "Expert" = "2",
                                           "Master" = "3", "Grandmaster" = "4")),
@@ -144,13 +86,61 @@ test <- vroom(here::here("Categorical Feature Challenge 2/Data/test.csv")) %>%
          ord_4 = map_dbl(ord_4, ~strtoi(charToRaw(.), 16L)),
          ord_5 = map_dbl(ord_5, ~strtoi(charToRaw(.), 16L) %>%
                            reduce(`+`))) %>%
-  mutate_at(vars(matches("ord|day|month")), as.numeric) %>%
-  mutate_at(vars(-matches("ord|day|month")), as.factor) %>%
+  mutate_at(vars(matches("ord")), as.numeric) %>%
+  mutate_at(vars(-matches("ord")), as.factor) %>%
   mutate_if(is.factor, ~fct_explicit_na(., na_level = "Missing"))
 
-test_prep <- final_rec %>%
-  prep() %>%
+#setting our recipe
+preproc_recipe <- train %>%
+  select(-id) %>%
+  recipe(target ~ .) %>%
+  step_meanimpute(all_numeric()) %>%
+  step_center(all_numeric()) %>%
+  step_scale(all_numeric()) %>%
+  step_other(all_nominal(), threshold = .03) %>%
+  step_dummy(all_nominal(), -all_outcomes()) %>%
+  step_interact(terms = ~starts_with("bin"):starts_with("ord")) %>%
+  prep()
+
+train_prep <- juice(preproc_recipe)
+
+test_prep <- preproc_recipe %>%
   bake(test)
+
+#bootstrapping cv
+train_cv <- train_prep %>%
+  bootstraps(times = 10, strata = "target")
+
+  
+glmnet_mod <- logistic_reg(
+  mode = "classification",
+  penalty = tune(),
+  mixture = tune()
+) %>%
+  set_engine("glmnet")
+
+#establishing a tuning grid for the glmnet hyperparameters
+glmnet_hypers <- parameters(penalty(), mixture()) %>%
+  grid_max_entropy(size = 20)
+
+#training the model
+glmnet_tuned_results <- tune_grid(
+  formula = target ~ .,
+  model = glmnet_mod,
+  resamples = train_cv,
+  grid = glmnet_hypers,
+  metrics = metric_set(roc_auc),
+  control = control_grid()
+)
+
+#extracting the best model parameters
+best_glmnet_params <- glmnet_tuned_results %>%
+  select_best()
+
+#finalizing the model
+glmnet_final_mod <- glmnet_mod %>%
+  finalize_model(parameters = best_glmnet_params) %>%
+  fit(target ~ ., data = train_prep)
 
 #predicting
 glmnet_prob <- predict(glmnet_final_mod, new_data = test_prep, type = "prob")
@@ -161,4 +151,17 @@ glmnet_preds <- tibble(
   target = glmnet_prob$.pred_1
 )
 
-write_csv(glmnet_preds, here::here("Categorical Feature Challenge 2/Submissions/glmnet_update_3_24.csv"))
+write_csv(glmnet_preds, here::here("Categorical Feature Challenge 2/Submissions/glmnet_update_3_21.csv"))
+
+###to do
+#1. Encode ordinal data differently
+#2. Mean impute ordinaldata
+#3. Give explicit NA for binary and nominal data
+#4. Look for interactions?
+
+
+##To consider for later
+#1. How to handle missing values other than just assigning them a separate factor level.
+#2. Look into other potentially useful recipe steps
+#3. Using vfold_cv right now, but can try bootstrapping later
+#4. Look into bayesian approach for hyperparameters?
