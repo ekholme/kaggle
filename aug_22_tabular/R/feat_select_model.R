d723f9ccc1e99e10450b17baccdd59e99c612090
Eric Ekholm <eric.ekholm@gmail.com>
2022-08-19 10:06:22 -0400

    normalize feats lasso spline

--- aug_22_tabular/R/feat_select_model.R
+++ aug_22_tabular/R/feat_select_model.R
@@ -7,13 +7,6 @@ trn <- read_csv(here::here("aug_22_tabular/data/train.csv")) |>
 
 tst <- read_csv(here::here("aug_22_tabular/data/test.csv"))
 
-# Set Up Initial Split --------------------
-
-splits <- initial_split(trn, prop = .8)
-
-trn1 <- training(splits)
-val <- testing(splits)
-
 # Set Up Initial Recipe -------------------
 
 rec <- recipe(failure ~ ., data = trn) |>
@@ -21,6 +14,7 @@ rec <- recipe(failure ~ ., data = trn) |>
     step_impute_mode(all_nominal_predictors()) |>
     step_impute_median(all_numeric_predictors()) |>
     step_YeoJohnson(all_numeric_predictors()) |>
+    step_normalize(all_numeric_predictors()) |>
     step_dummy(all_nominal_predictors())
 
 # Set Up Initial Model ---------------------
@@ -50,20 +44,21 @@ feat_pull <- wf_fit |>
 # ok so loading seems to be the only thing that matters
 # let's try a spline model
 
-only_loading <- select(trn, failure, loading)
+good_feats <- select(trn, failure, loading, measurement_17)
 
-new_rec <- recipe(failure ~ ., data = only_loading) |>
-    step_impute_median(loading) |>
-    step_YeoJohnson(loading) |>
-    step_ns(loading, deg_free = tune())
+new_rec <- recipe(failure ~ ., data = good_feats) |>
+    step_impute_median(all_numeric_predictors()) |>
+    step_YeoJohnson(all_numeric_predictors()) |>
+    step_normalize(all_numeric_predictors()) |>
+    step_ns(all_numeric_predictors(), deg_free = tune()) 
 
 spline_wf <- workflow() |>
     add_recipe(new_rec) |>
     add_model(spec)
 
-df_grid <- grid_regular(deg_free(c(2, 6)), levels = 5)
+df_grid <- grid_regular(deg_free(c(1, 5)), levels = 5)
 
-cvs <- vfold_cv(only_loading, v = 5)
+cvs <- vfold_cv(good_feats, v = 5)
 
 # Tune DF -----------------------------
 
@@ -76,21 +71,21 @@ spline_res <- spline_wf |>
 # Examine Fits -----------------------
 
 spline_res |>
-    show_best("accuracy")
+    show_best("roc_auc")
 
 
 best_fits <- spline_res |>
-    select_best("accuracy")
+    select_best("roc_auc")
 
 # finalize wf
 final_wf <- spline_wf |>
     finalize_workflow(best_fits)
 
 final_fit <- final_wf |>
-    fit(data = only_loading)
+    fit(data = good_feats)
 
 # predict
-preds <- predict(final_fit, new_data = data.frame(loading = tst$loading), type = "prob")
+preds <- predict(final_fit, new_data = tst, type = "prob")
 
 #make submission
 sub <- tibble(
@@ -98,4 +93,4 @@ sub <- tibble(
     failure = preds$.pred_1
 )
 
-write_csv(sub, here::here("aug_22_tabular/submissions/lasso_spline.csv"))
+write_csv(sub, here::here("aug_22_tabular/submission/lasso_spline.csv"))
