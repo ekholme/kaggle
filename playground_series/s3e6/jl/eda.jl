b30a0d439a32d5f9b12a35aba8859727232ba169
Eric Ekholm <eric.ekholm@gmail.com>
2023-02-14 16:24:03 -0500

    start jl eda

--- playground_series/s3e6/jl/eda.jl
+++ playground_series/s3e6/jl/eda.jl
@@ -17,4 +17,32 @@ mapcols(x -> sum(ismissing, x), trn)
 # target column --------------
 target = trn.:price
 
-##RESUME HERE
+mean(target)
+
+#and let's get a histogram of the target
+hist(target)
+#ok, so a fairly uniform distribution
+
+# binary features --------------
+
+binary_features = [:hasYard, :hasPool, :isNewBuilt, :hasStormProtector, :hasStorageRoom]
+
+trn_bin = copy(trn[:, binary_features])
+
+mapcols(x ->sum(x) / nrow(trn), trn_bin)
+
+#ok, so about 45-47% of all houses have any given one of these features.
+
+#faceted histograms
+#see https://docs.makie.org/stable/tutorials/layout-tutorial/ for a start
+f = Figure()
+ax = Axis(f[1,1])
+
+demo_cols = [:squareMeters, :numberOfRooms, :basement]
+
+for i in demo_cols
+    hist!(ax, trn[:, i])
+end
+
+f
+#this doesn't work yet
