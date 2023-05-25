11a7a66bc5b0c7ffea8172321edeed3ba13f5aa8
Eric Ekholm <eric.ekholm@gmail.com>
2023-05-25 15:44:20 -0400

    pca in sklearn pipeline

--- age_related_conditions/py/pca.py
+++ age_related_conditions/py/pca.py
@@ -1,11 +1,13 @@
 import polars as pl
 import numpy as np
+import pandas as pd
 from sklearn.metrics import log_loss
 from sklearn.linear_model import LogisticRegression
 from sklearn import preprocessing
 from sklearn.impute import SimpleImputer
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import Pipeline
 from sklearn.decomposition import PCA
+from sklearn.compose import ColumnTransformer
 
 trn = pl.read_csv("./age_related_conditions/data/trn.csv")
 val = pl.read_csv("./age_related_conditions/data/validation.csv")
@@ -16,15 +18,53 @@ oh = preprocessing.OneHotEncoder(drop='first')
 pca = PCA()
 lr = LogisticRegression(C=.9)
 
-pre_pipe = make_pipeline(imp, yj, oh, pca)
 
 #finagle X a little bit
 X = trn.select(pl.exclude(['Id', 'Class']))
 y = trn['Class']
 
-#fitting pre-pipe -- want to get a sense of how many PCA components I should keep
+#defining a preprocessing pipeline
 
-pre_pipe.fit(X)
+cat_feats = ['EJ']
+num_feats = [i for i in X.columns if i not in set(cat_feats)]
+
+cat_transformer = Pipeline(
+    [
+        ('cat_imputer', SimpleImputer(
+            strategy='constant', fill_value='missing'
+        )),
+        ('onehot', preprocessing.OneHotEncoder(drop='first', handle_unknown='ignore'))
+    ]
+)
+
+num_transformer = Pipeline(
+    [
+        ('num_imputer', SimpleImputer(strategy='median', missing_values=np.nan)),
+        ('yj', preprocessing.PowerTransformer()),
+        ('pca', PCA())
+    ]
+)
+
+preprocessor = ColumnTransformer(
+    [
+        ('cats', cat_transformer, cat_feats),
+        ('nums', num_transformer, num_feats)
+    ],
+    remainder='drop'
+)
+
+#unfortunately, I think sklearn pipelines want pandas dataframes
+#so I need to do this conversion before running?
+Xpd = X.to_pandas()
+
+preprocessor.fit_transform(Xpd)
+
+ev_pct = preprocessor.named_transformers_['nums'].named_steps['pca'].explained_variance_ratio_.cumsum()
+
+
+out_pcs = sum(ev_pct < .95)
+#ok, so we want to keep this many components, I guess
+
 
 #see this article for more on preprocessing pipelines in sklearn
 #https://medium.com/analytics-vidhya/how-to-apply-preprocessing-steps-in-a-pipeline-only-to-specific-features-4e91fe45dfb8
